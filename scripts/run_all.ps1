dc9b53715500be9c5fb7e1850c10892d53947eb5
nuuuwan <nuuuwan@gmail.com>
2024-10-31 14:20:00 +0530

    run_all

--- scripts/run_all.ps1
+++ scripts/run_all.ps1
@@ -1,4 +1,12 @@
+git pull origin main
+
 python .\src\logos\PartyLogo.py
 
 cp images\white\*.png C:\Users\ASUS\Not.Dropbox\CODING\js_react\election\public\images\party_logos\
 ls C:\Users\ASUS\Not.Dropbox\CODING\js_react\election\public\images\party_logos\
+
+git add *
+
+git commit -m "run_all"
+
+git push origin main
